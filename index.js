b0b84dc681baed89dc0015a148ef0d155927a4df
nikitha26 <mnikitha365@gmail.com>
2020-12-20 13:38:12 +0530

    reset password

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function Resetpsw(){ 
     var psw = document.getElementById("password").value;
-    var reset = document.getElementById("resetpsw").value;     
-    // console.log(psw + " hi "+reset)
+    var reset = document.getElementById("resetpsw").value; 
+    
+    //Check given psw length and only letters
+    if(psw.length < 8 && !/^[a-zA-Z]*$/g.test(psw)){
+        document.getElementById("error1").innerHTML="Enter atleast 8 letters and Enter only letters"
+    }
+    //Check psw letters or digits
+    else if(!/^[a-zA-Z]*$/g.test(psw)){
+            document.getElementById("error1").innerHTML="Enter only letters"
+    }
+    //Check psw length
+    else if(psw.length < 8){
+        document.getElementById("error1").innerHTML="Enter atleast 8 letters"
+    }
+
+    else{
+        document.getElementById("valid").innerHTML="Valid input"
+    }
+
+    //Check psw match or not
     if(psw !== reset){
-       document.getElementById("error").innerHTML="Do not match";
-    }
-    if(psw.length <= 8){
-        console.log(psw.length)
-        console.log("Have error")
-    }
+        document.getElementById("error2").innerHTML="Password do not match";
+     }
+     else{
+         document.getElementById("validpsw").innerHTML="Match password"
+     }
 
 }
